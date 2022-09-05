ae7b6bf1fb9fe7ff50e8436c528faf0fe1bb74c3
kibuelll <kibuellyoi34@gmail.com>
2022-09-05 13:44:23 +0700

    feat: main navbar

--- src/components/main navbar/index.js
+++ src/components/main navbar/index.js
@@ -1,37 +1,8 @@
-import { Link } from "react-router-dom"
-import { Row, Col, Button, Dropdown, Menu} from 'antd'
+// import { Link } from "react-router-dom"
+import { Row, Col, Button, Dropdown} from 'antd'
 import "./index.scss"
 
-const menuDrop = (
-  <Menu
-    style={{ overflow: 'hidden', listStyle: 'none', listStyleType: 'none', color: 'black',textAlign:'right' }}
-    items={[
-      {
-        label:
-          <Link to={"/"} style={{color:'black',textDecoration:'none',marginRight:'8px'}}>
-            Home
-          </Link>,
-        key: '0',
-      },
-      {
-        label:
-          <Link to={"/"} style={{color:'black',textDecoration:'none',marginRight:'8px'}}>
-            Model
-          </Link>,
-        key: '1',
-      },
-      {
-        label:
-          <Link to={"/"} style={{color:'black',textDecoration:'none',marginRight:'10px'}}>
-            Pricelist
-          </Link>,
-        key: '2',
-      }
-    ]}
-  />
-)
-// const modelDrop = ([])
-
+const menuDrop =([])
 export default function MainNavbar() {
   return (
     <Row style={{
@@ -39,11 +10,11 @@ export default function MainNavbar() {
     }}>
       <Col>
         <Row>
-          <Col xs={{span:24}} className="logo">
+          <Col xs={{ span: 24 }} span={24} className="logo">
             <img className="mainlogo"
-              src="https://hyundai-official.com/wp-content/uploads/2022/01/logohyundai.png" 
+              src="https://hyundai-official.com/wp-content/uploads/2022/01/logohyundai.png"
               alt="not found"
-              />
+            />
           </Col>
           <Col className="address">
             <span>
@@ -55,8 +26,8 @@ export default function MainNavbar() {
           </Col>
         </Row>
         <Row className="navbar-menu">
-          <Dropdown overlay={menuDrop} overlayStyle={{ height: '70px', backgroundColor: 'rgba(74, 90, 124, 0.89)',width:'fit-content',fontFamily:'inherit', transform:'translateX(-5px)', borderRadius:'10px',color:'white'}} placement={"bottomRight"}>
-            <Button className="menubtn">MENU</Button>
+          <Dropdown overlay={menuDrop} overlayStyle={{ height: '70px', backgroundColor: 'rgba(74, 90, 124, 0.89)', width: 'fit-content', fontFamily: 'inherit', transform: 'translateX(-5px)', borderRadius: '10px', color: 'white' }} placement={"bottomRight"}>
+            <Button className="menubtn" >MENU</Button>
           </Dropdown>
         </Row>
       </Col>
