0ba96a03a5d32277b8f7f40c4db21683691ad42e
Fauzan Bintang <obin.aji@gmail.com>
2022-09-06 14:12:12 +0700

    add main footer component

--- src/components/main footer/index.js
+++ src/components/main footer/index.js
@@ -1,5 +1,16 @@
+import { Row, Col } from "antd";
+
 export default function MainFooter() {
   return (
-    <></>
-  )
+    <Row>
+      <Col span={24} style={{
+        background: "rgba(0,0,0,0.3)",
+        textAlign: "center",
+        padding: "2.5em",
+        color: "#FCFCFC"
+      }}>
+        Hyundai Andalan - Dealer Resmi Hyundai
+        </Col>
+    </Row>
+  );
 }
