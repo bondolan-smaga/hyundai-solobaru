051858eef499a57e3c8c0b9835698e09454cd2bf
kibuelll <kibuellyoi34@gmail.com>
2022-09-07 15:59:04 +0700

    feat: component CS card

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,10 +1,12 @@
-import { MainLayout, CarouselBanner} from "../../components";
+import { MainLayout, CarouselBanner } from "../../components";
+import CsCard from "../../components/CS-card";
 
 
 export default function HomePage() {
   return (
     <MainLayout>
       <CarouselBanner />
+      <CsCard/>
     </MainLayout>
   );
 }
